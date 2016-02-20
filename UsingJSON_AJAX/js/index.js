ee76ca03325af20b19d995809c18243b23ef285f
Funke Makanjuola <mak_funke@yahoo.com>
2016-02-19 19:53:10 -0500

    Update index.js

--- UsingJSON_AJAX/js/index.js
+++ UsingJSON_AJAX/js/index.js
@@ -6,7 +6,7 @@
     */
     var states = document.getElementById('state');
     var counries = document.getElementById('countries');
-    var _msg_error = document.getElementById('msg_error');
+    
 
 
     /*
@@ -15,45 +15,38 @@
      * @param {type} Url
      * @param {type} target
      * Url - specify location of data
-     * terger - specify location to load data
-     use AJAX to populate drop down list by loading external file */
-    function loadData(Url, target) {
-        /*create a new XMLHttpRequest object request*/
-        var xhr = new XMLHttpRequest();
+     * target - specify location to load data
+     * use AJAX to populate drop down list by loading external file */
+    function loadData(Url, obj) {
+        /*create a new XMLHttpRequest object*/
+        var xmlhttp = new XMLHttpRequest();
+        /*
+         * Function to check if state of request has changed. Checking the response status.
+         * onreadystatechange is triggered whenever readystate changes
+         * 4 - request finished and response is ready 
+         * 2XX - Check for successful http request
+         
+         */
+        xmlhttp.onreadystatechange = function () {
+            if ((xmlhttp.readyState === 4) && (xmlhttp.status === 200)) {
+
+                //Get response body as string 
+                obj.innerHTML += xmlhttp.responseText;
+            }
+
+        };
 
         /*
          * Create a GET Request used to get inforamation using the parameter-based  URL.
          * The request is set true which means request should be asynchronous
          */
-        xhr.open('GET', Url, true);
+        xmlhttp.open('GET', Url, true);
 
-        /*
-         * Function to check if state of request has changed, checking the response status.
-         * onreadystatechange is triggered whenever readystate changes
-         * 4 - request finished and response is ready 
-         * 2XX - Check for successful http request
-         * 3XX - Check if the resource has not been modified
-         */
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
 
-                    //Get response body as string 
-                    target.innerHTML += xhr.responseText;
-                } else {
-                    //Get http text that corresponse with the status
-                    console.log(xhr.statusText);
-
-                    // display error message
-                    _msg_error.innerHTML += '<p class="msgerror">Error getting ' +
-                                  target.name + ": " + xhr.statusText + ",code: " + xhr.status + "</p>";
-                }
-            }
-        };
         /*
          * Finally send the request
          */
-        xhr.send();
+        xmlhttp.send();
     }
 
     //load the external files 	
